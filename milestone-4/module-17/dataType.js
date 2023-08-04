455eb58aae69e001ded9867896ecdf793ed7717f
Mahaburalam <mahaburcse@gmail.com>
2023-08-04 10:05:50 +0600

    variableDataType

---
+++ milestone-4/module-17/dataType.js
@@ -0,0 +1,15 @@
+var number = 121;
+console.log(typeof(number));
+
+var name = "Mahabur Alam"
+console.log(typeof(name));
+
+var visitKhulna = false
+console.log(typeof(visitKhulna));
+
+var phone;
+console.log(phone);
+
+var money = null
+console.log(typeof(money));
+
---
+++ milestone-4/module-17/variable.js
@@ -0,0 +1,11 @@
+var name = "Mahabur";
+console.log("My Name is:", name);
+console.log(typeof(name));
+
+var number = 20;
+console.log(number);
+console.log(typeof(number));
+
+var weight = 73.4;
+console.log(weight);
+console.log(typeof(weight));
