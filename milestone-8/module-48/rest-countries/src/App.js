596ed8ad520422b7934db66dab3d80275f4f5d8d
Mahaburalam <mahaburcse@gmail.com>
2023-08-12 23:55:35 +0600

    Load Api Data

--- milestone-8/module-48/rest-countries/src/App.js
+++ milestone-8/module-48/rest-countries/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+function LoadCountries(){
+  // load rest countries api data
+  const [countries, setCountries] = useState([]);
+  useEffect( ()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response=>response.json())
+    .then(data=>setCountries(data))
+  }, [])
+
+  return(
+    <div>
+      <h2>Visiting Rest Of The Countries In The World.</h2>
+      <h3>Total Number Of Countries: { countries.length }</h3>
+    </div>
+  )
 }
 
 export default App;
