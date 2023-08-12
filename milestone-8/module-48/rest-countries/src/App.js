b4fc7e917f78802b537dac40714051c9c81dd84f
Mahaburalam <mahaburcse@gmail.com>
2023-08-13 00:20:50 +0600

    View Api data

--- milestone-8/module-48/rest-countries/src/App.js
+++ milestone-8/module-48/rest-countries/src/App.js
@@ -23,6 +23,18 @@ function LoadCountries(){
     <div>
       <h2>Visiting Rest Of The Countries In The World.</h2>
       <h3>Total Number Of Countries: { countries.length }</h3>
+      {
+        countries.map( country => <CountryView official={country.name.official} language={country.capital}></CountryView>)
+      }
+    </div>
+  )
+}
+
+function CountryView(props){
+  return(
+    <div>
+      <h4>Country Official Name: {props.official}</h4>
+      <p>Language: {}</p>
     </div>
   )
 }
