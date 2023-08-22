386cda7b0539649db76b12ae33fb96e52a8dacd7
Mahaburalam <mahaburcse@gmail.com>
2023-08-22 06:50:14 +0600

    propsandcomponenet

--- milestone-8/module-48.5/react-concept-recap/src/App.js
+++ milestone-8/module-48.5/react-concept-recap/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,21 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Country name='Bangladesh' region='Asia'></Country>
+      <Country name='Germany' region='Europe'></Country>
+      <Country name='Canada' region="North America"></Country>
     </div>
   );
+}
+
+function Country(props){
+  return(
+    <div className='bg-success m-4 p-4 text-white'>
+      <h3>Country Name: {props.name}</h3>
+      <h4>Country Region: {props.region}</h4>
+    </div>
+  )
 }
 
 export default App;
