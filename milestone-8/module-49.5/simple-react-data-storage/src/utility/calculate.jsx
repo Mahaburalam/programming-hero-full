4e2ea1059b3d5f69418615818cffbbb5431cc755
Mahaburalam <mahaburcse@gmail.com>
2023-08-26 07:48:38 +0600

    Import and Export

---
+++ milestone-8/module-49.5/simple-react-data-storage/src/utility/calculate.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+const Addition = (first, second) => {
+    return (first + second);
+}
+
+const Multiplication = (first, second) => {
+    const output = first * second;
+    return (output);
+}
+
+// export default Addition;
+export {Multiplication, Addition}
