1f40e960da093f97d30f14cacf61d65c02140c5c
Mahaburalam <mahaburcse@gmail.com>
2023-10-07 22:15:25 +0600

    project started

---
+++ milestone-8/module-50/simple e-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header-style'>
+            <h4>This is header</h4>
+        </div>
+    );
+};
+
+export default Header;
