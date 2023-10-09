67119410591ea11d399de1f5e72d140029d3a7d0
Mahaburalam <mahaburcse@gmail.com>
2023-10-09 13:18:29 +0600

    header and nav menu done

--- milestone-8/module-50/simple e-commerce/src/components/Header/Header.jsx
+++ milestone-8/module-50/simple e-commerce/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import "./Header.css";
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div className='header-style'>
-            <h4>This is header</h4>
-        </div>
+        <nav className='header-style'>
+            <img src={logo} alt="website main logo" />
+            {/* <h4>This is header</h4> */}
+            <div className='nav-menu'>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/manage-inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
