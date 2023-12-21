eeb3084bc830abfe3abe2466d3bcdd631794a759
Mahaburalam <mahaburcse@gmail.com>
2023-12-21 09:42:14 +0600

    Props

--- milestone-8 recap/module 47/my-react-app/src/functional-component.jsx
+++ milestone-8 recap/module 47/my-react-app/src/functional-component.jsx
@@ -5,15 +5,13 @@ function App() {
     <>
       <div>
         <h1>This is my personal info</h1>
-        <h3>Dynamic Component, pass data through Props</h3>
         {/* component call */}
-        {/* pass dynamic data through props in component*/}
-        <Myinfo name="Md Mahabur Alam" prof="Software Developer" country='Bangladesh' homeDis="Khulna"></Myinfo>
-        <Myinfo name="Germany" prof="Europe" country="Test" homeDis="New Test"></Myinfo>
-        {/* <p>This is my family info</p> */}
+        <Myinfo></Myinfo>
+        <Myinfo></Myinfo>
+        <Myinfo></Myinfo>
+        <p>This is my family info</p>
         {/* another component call */}
         <h1>This is my family background</h1>
-        <h3>Static Component</h3>
         <Mybio></Mybio>
         <Mybio></Mybio>
         <Mybio></Mybio>
@@ -23,14 +21,13 @@ function App() {
 }
 
 // finctional component
-function Myinfo(props){
-  console.log(props);
+function Myinfo(){
   return(
     <div className='my-bio'>
-      <h3>Name: {props.name}</h3>
-      <h5>Profession: {props.prof}</h5>
-      <p>Country: {props.country}</p>
-      <p>Home District: {props.homeDis}</p>
+      <h2>Name: Md Mahabur Alam</h2>
+      <h4>Profession: Developer</h4>
+      <p>Country: Bangladesh</p>
+      <p>Home District: Khulna</p>
     </div>
   )
 }
