890c9af7621a1f1494746e7b55b64348c7cd6eb3
Mahaburalam <mahaburcse@gmail.com>
2024-04-21 09:31:18 +0600

    mostly use dom event

---
+++ milestone-5/module-25/js/dom-event-summery.js
@@ -0,0 +1,9 @@
+// option:1
+function onClickHandler(){
+    document.getElementById('change-text').innerText = 'The transparent keyword represents a fully transparent color. This makes the background behind the colored item completely visible. Technically, transparent is a shortcut for rgb(0 0 0 / 0%).'
+}
+
+// option:2
+document.getElementById('addEventListener').addEventListener('click',function(){
+    document.getElementById('change-text').innerText = 'If after checking these points the issue persists, please provide more details about your HTML structure and any other relevant CSS or JavaScript code so that I can help you further.';
+})
