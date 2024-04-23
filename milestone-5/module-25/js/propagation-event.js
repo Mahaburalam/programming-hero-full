4795a8e63cbcbf636dd9e09749c29af5687f61bb
Mahaburalam <mahaburcse@gmail.com>
2024-04-23 15:33:17 +0600

    dom event

---
+++ milestone-5/module-25/js/propagation-event.js
@@ -0,0 +1,11 @@
+const secParagraph = document.getElementById('item-2').addEventListener('click', function(){
+    console.log('Clicked second paragraph');
+})
+
+const divSection = document.getElementById('div-section').addEventListener('click',function(){
+    console.log('Div Section Clicked');
+});
+
+const mainSection = document.getElementById('main-section').addEventListener('click', function(){
+    console.log('main section clicked');
+})
