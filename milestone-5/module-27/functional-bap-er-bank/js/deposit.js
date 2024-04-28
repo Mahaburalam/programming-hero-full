8436c1100261198eb04faf9803dbc069c5398c09
Mahaburalam <mahaburcse@gmail.com>
2024-04-29 01:04:34 +0600

    functional new deposit and current deposit

--- milestone-5/module-27/functional-bap-er-bank/js/deposit.js
+++ milestone-5/module-27/functional-bap-er-bank/js/deposit.js
@@ -1,3 +1,28 @@
+// get data of input field
+function getInputFieldById(valueId){
+    const inputElement = document.getElementById(valueId);
+    const inputElementToValue = inputElement.value;
+    const inputValueToNumber = Number(inputElementToValue);
+    inputElement.value = '';
+    return inputValueToNumber;
+
+    // after function return, console is not working;
+    // console.log(inputValueToNumber, typeof(inputValueToNumber));
+}
+
+// get data of element field
+function getElementById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValue = element.innerText;
+    const ElementAmount = Number(elementValue);
+    return ElementAmount;
+}
+
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    
+    const newDepositAmount = getInputFieldById('deposit-input');
+    // console.log(newDepositAmount);
+    const currentElementAmount = getElementById('deposit-amount');
+    // console.log(currentElementAmount);
+    const currentTotalBalance = getElementById("current-balance");
+    // console.log(currentTotalBalance);
 })
