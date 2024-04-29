4687b2d2b50222f17c6b0e6cd062081327915495
Mahaburalam <mahaburcse@gmail.com>
2024-04-29 09:24:31 +0600

    calculate and update the value

--- milestone-5/module-27/functional-bap-er-bank/js/deposit.js
+++ milestone-5/module-27/functional-bap-er-bank/js/deposit.js
@@ -18,11 +18,25 @@ function getElementById(elementId){
     return ElementAmount;
 }
 
+// set or update amount in element field
+function setElementAmount(elementId, newValue){
+    const getUpdatedElementValue = document.getElementById(elementId);
+    getUpdatedElementValue.innerText = newValue;
+}
+
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const newDepositAmount = getInputFieldById('deposit-input');
     // console.log(newDepositAmount);
-    const currentElementAmount = getElementById('deposit-amount');
+    const currentDepositAmount = getElementById('deposit-amount');
     // console.log(currentElementAmount);
     const currentTotalBalance = getElementById("current-balance");
     // console.log(currentTotalBalance);
+
+    const updatedDepositAmount = currentDepositAmount + newDepositAmount;
+    setElementAmount('deposit-amount', updatedDepositAmount);
+
+    const updatedBalanceAmount = currentTotalBalance + newDepositAmount;
+    setElementAmount('current-balance', updatedBalanceAmount);
+
+
 })
