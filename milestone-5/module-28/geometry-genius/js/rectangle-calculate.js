abf21b047662df201b7d404771acd3e6b524ad2a
Mahaburalam <mahaburcse@gmail.com>
2024-05-01 20:51:36 +0600

    calculate rectangle area

---
+++ milestone-5/module-28/geometry-genius/js/rectangle-calculate.js
@@ -0,0 +1,21 @@
+function rectangleCalculation(){
+    // document.getElementById('rectangle-btn');
+    // console.log('4444444');
+
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidth = rectangleWidthInput.value;
+    rectangleWidthInput.value = '';
+
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLength = rectangleLengthInput.value;
+    rectangleLengthInput.value = '';
+
+    // calculate rectangle area 
+    const rectangleArea = rectangleWidth * rectangleLength;
+    // console.log(rectangleArea);
+
+    // show rectangle area
+    const showRectangleArea = document.getElementById('rectangle-result')
+    showRectangleArea.innerText = rectangleArea;
+
+}
