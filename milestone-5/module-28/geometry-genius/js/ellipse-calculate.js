4d4930ffd84eeaae3c2860447cfaa58c0831c910
Mahaburalam <mahaburcse@gmail.com>
2024-05-02 00:42:57 +0600

    ellipse area

---
+++ milestone-5/module-28/geometry-genius/js/ellipse-calculate.js
@@ -0,0 +1,11 @@
+function ellipseCalculate(){
+    const ellipseArea = getInputField('ellipse-area');
+    const ellipseBase = getInputField('ellipse-base');
+    // console.log(ellipseArea, ellipseBase);
+
+    // calculate ellipse area
+    const ellipseResult = 3.1416 * ellipseArea * ellipseBase;
+
+    // show ellipse area result
+    showAreaResult('ellipse-result', ellipseResult)
+}
