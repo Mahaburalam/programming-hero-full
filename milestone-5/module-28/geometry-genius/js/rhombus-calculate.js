4713f557dd1a7bfa8dcb20972683381264ca9c69
Mahaburalam <mahaburcse@gmail.com>
2024-05-02 00:26:24 +0600

    rhombus area calculate

---
+++ milestone-5/module-28/geometry-genius/js/rhombus-calculate.js
@@ -0,0 +1,10 @@
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+    const rhombusFirstDimension = getInputField('rhombus-first-dimension');
+    const rhombusSecondDimension = getInputField('rhombus-second-dimension');
+
+    // calculate rhombus area
+    const rhombusArea = .5 * rhombusFirstDimension * rhombusSecondDimension;
+
+    // show rhombus area
+    showAreaResult("rhombus-result", rhombusArea);
+})
