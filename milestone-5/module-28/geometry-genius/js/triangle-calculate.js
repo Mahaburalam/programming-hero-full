26c205452cfdb1a05bf314cf7da1dc537c95d32b
Mahaburalam <mahaburcse@gmail.com>
2024-05-01 20:08:11 +0600

    triangle area calcuate done

--- milestone-5/module-28/geometry-genius/js/triangle-calculate.js
+++ milestone-5/module-28/geometry-genius/js/triangle-calculate.js
@@ -1,3 +1,23 @@
 document.getElementById('triangle-btn').addEventListener('click', function(){
-    console.log('working...')
+    // console.log('working...')
+    
+    // get triangle base
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseValue = triangleBaseInput.value;
+    triangleBaseInput.value = '';
+    // console.log(triangleBaseValue);
+
+    // get triangle height
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightValue = triangleHeightInput.value;
+    triangleHeightInput.value = '';
+    // console.log(triangleHeightValue);
+
+    // calculate triangle area
+    const triangleArea = .5 * triangleBaseValue * triangleHeightValue;
+    // console.log(triangleArea)
+
+    // show triangle area
+    const showTriangleArea = document.getElementById('area-result');
+    showTriangleArea.innerText = triangleArea;
 })
