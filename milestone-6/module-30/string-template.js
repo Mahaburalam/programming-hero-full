3e61ceb416d4f513fcb484fc3fb6bbe7458b5087
Mahaburalam <mahaburcse@gmail.com>
2024-05-04 12:44:13 +0600

    template string/ string literal

---
+++ milestone-6/module-30/string-template.js
@@ -0,0 +1,12 @@
+const myLifeStory = `I'm Mahabur Alam.
+I completed by B.sc in CSE.
+Now, i'm working a private company as a senior programmer.`;
+console.log(myLifeStory);
+
+const FirstNumber = 23;
+const secondNumber = 12;
+
+console.log(`The sum of two number is : ${FirstNumber + secondNumber} and type of this number is ${typeof(FirstNumber + secondNumber)}`);
+
+console.log("The sum of two number is: ", (FirstNumber + secondNumber), "and type id this number is:", typeof(FirstNumber + secondNumber));
+// console.log(FirstNumber);
