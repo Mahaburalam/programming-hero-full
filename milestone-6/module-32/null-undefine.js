7be173f6da25b1336a48e70a192fb9ff53ecf4cc
Mahaburalam <mahaburcse@gmail.com>
2024-05-13 05:43:18 +0600

    null and undefine

---
+++ milestone-6/module-32/null-undefine.js
@@ -0,0 +1,19 @@
+// way to find undefine
+const value = null;
+const test = undefined;
+
+// console.log(value, ', type of null is:', typeof(value), ' - ', test, ', and type of Undefine is:', typeof(test));
+
+let newValue;
+// console.log(newValue);
+
+function myFunc(a, b){
+    console.log(a, b);
+}
+// myFunc();
+
+function testFunc(a, b, c){
+    const sum = (a + b + c);
+    console.log( a, b, c)
+}
+// testFunc(2);
