1e8f58b597daccda58857f9568ef1d3c4ed4d554
Mahaburalam <mahaburcse@gmail.com>
2024-05-15 22:51:31 +0600

    closure

--- milestone-6/module-32/closure.js
+++ milestone-6/module-32/closure.js
@@ -1,1 +1,14 @@
 // important
+function country(){
+    division = 0;
+    return function(){
+        division ++;
+        return division;
+    }
+}
+const firstDivision = country();
+console.log('For First',firstDivision());
+
+const secondDivision = country();
+console.log('For Second',secondDivision());
+console.log('For Second',secondDivision());
