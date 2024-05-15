2cb97ba242f6340012706152cf3ddb3852bc635a
Mahaburalam <mahaburcse@gmail.com>
2024-05-15 08:56:39 +0600

    double and triple equal

---
+++ milestone-6/module-32/equal.js
@@ -0,0 +1,16 @@
+const value = 23
+const value1 = "23";
+const value2 = 23
+const value3 = 1
+const value4 = false
+const value5 = 0
+const value6 = true
+
+if(value3 === value6)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
