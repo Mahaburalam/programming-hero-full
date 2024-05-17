e893ae2b59c24e2fcc116ed0fd1a31d51d2b58aa
Mahaburalam <mahaburcse@gmail.com>
2024-05-17 15:04:13 +0600

    api introduction

---
+++ milestone-6/module-33/api.js
@@ -0,0 +1,19 @@
+const persons = [
+    {
+        fullName: "Mahabur Alam",
+        age: 27,
+        occupation: "Service"
+    },
+    {
+        fullName: "Nusrat Jahan",
+        age: 27,
+        occupation: "House Wife"
+    },
+    {
+        fullName: "Ahnaf Anas",
+        age: 1.5,
+        occupation: "Playing"
+    }
+];
+
+console.table(persons);
