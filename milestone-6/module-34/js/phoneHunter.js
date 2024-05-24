166630b4be049249942b61e370967ab802ae7c19
Mahaburalam <mahaburcse@gmail.com>
2024-05-24 21:15:05 +0600

    show phone list

--- milestone-6/module-34/js/phoneHunter.js
+++ milestone-6/module-34/js/phoneHunter.js
@@ -1,16 +1,46 @@
-// console.log("test")
-const loadPhone = async () =>{
-    const url = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    const res = await url.json();
-    const data = res.data;
-    // console.log(data);
-    displayPhone(data)
+const loadPhone = async () => {
+    const fetchPhone = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const response = await fetchPhone.json();
+    // console.log(response);
+    const data = response.data;
+    displayPhones(data);
 }
 
-const displayPhone = (phones) => {
-    phones.forEach(phone =>{
-        console.log(phone);
+const displayPhones = (phones) => {
+    // console.log(phones);
+    // step:1
+    const phoneContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        // console.log(phone);
+
+        // step:2
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = 'card p-4 bg-base-100 shadow-xl border-2 ms-5';
+
+        // step:3
+        phoneCard.innerHTML = `
+            <figure class="mt-5">
+                <img src="${phone.image}" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>${phone.slug}</p>
+                <h3 class='text-2xl text-center font-semibold'>$999</h3>
+                <div class="card-actions justify-center px-5 mt-4">
+                    <button class="btn btn-primary text-2xl">Show Details</button>
+                </div>
+            </div>
+        `
+        // step:4
+        phoneContainer.appendChild(phoneCard);
+
     })
+
+}
+
+// search option 
+const buttonSearch = () => {
+    console.log("search button clicked")
 }
 
 loadPhone();
