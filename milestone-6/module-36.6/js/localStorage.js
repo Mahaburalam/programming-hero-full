f02d3ecb8da776dbb3c632cb4db4b813bce70df9
Mahaburalam <mahaburcse@gmail.com>
2024-05-29 09:43:01 +0600

    local storage set item

---
+++ milestone-6/module-36.6/js/localStorage.js
@@ -0,0 +1,7 @@
+const setNewItem = localStorage.setItem("Person", "Hasan Mahmud");
+console.log(setNewItem);
+
+const setItem = () =>{
+    localStorage.setItem("Person", "Hasan Mahmud");
+    localStorage.setItem("age", 23);
+}
