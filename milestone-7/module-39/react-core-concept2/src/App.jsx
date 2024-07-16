d375dc0c7f176f78d7f13aeb7544218870b6ae3a
mahaburalam <mahaburcse@gmail.com>
2024-07-16 09:02:42 +0600

    state

--- milestone-7/module-39/react-core-concept2/src/App.jsx
+++ milestone-7/module-39/react-core-concept2/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css'
 function App() {
+  const eventHandler = () => {
+    alert("check your name");
+    }
+
+    const [count, setCount] = useState(0);
+
+    const addCount = () => {
+      const newAddCount = count + 1;
+      // console.log(newCount);
+      setCount(newAddCount);
+    }
+
+    const reduceCount = () => {
+      const newReduceCount = count - 1;
+      setCount(newReduceCount);
+    }
 
   return (
     <>
       <h1>Vite + React</h1>
+      <button onClick={eventHandler}>Click</button>
+      <br></br>
+      <div>
+        <h3>Count: {count}</h3>
+        <button style={{marginRight:"12px"}} onClick={addCount}>Add</button>
+        <button onClick={reduceCount}>Reduce</button>
+      </div>
     </>
   )
 }
